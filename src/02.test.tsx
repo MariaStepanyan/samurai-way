eee3c6b6c77736c9ed58f997c0f08c06dd018e2c
MariaStepanyan <drmariastepanyan@gmail.com>
2024-01-08 19:57:48 +0300

    test

---
+++ src/02.test.tsx
@@ -0,0 +1,60 @@
+// let city: Citytype;
+
+// beforeEach(() => {
+//   city = {
+//     houses: [
+//       {
+//         buildedAt: 2012,
+//         repaired: false,
+//         address: {
+//           number: 100,
+//           street: {
+//             title: "White street"
+//           }
+//         }
+//       },
+//       {
+//         buildedAt: 2008,
+//         repaired: false,
+//         address: {
+//           number: 100,
+//           street: {
+//             title: "Happy street"
+//           }
+//         }
+//       },
+//       {
+//         buildedAt: 2020,
+//         repaired: false,
+//         address: {
+//           number: 200,
+//           street: {
+//             title: "Hogwarts street"
+//           }
+//         }
+//       }
+//     ],
+//   }
+// })
+
+
+// test("city should contains 3 houses", () => {
+//   expect(city.houses.length).toBe(3);
+
+//   expect(city.houses[0].buildedAt).toBe(2012);
+//   expect(city.houses[0].repaired).toBe(false);
+//   expect(city.houses[0].address.number).toBe(100);
+//   expect(city.houses[0].address.street.title).toBe("White street");
+
+//   expect(city.houses[1].buildedAt).toBe(2008);
+//   expect(city.houses[1].repaired).toBe(false);
+//   expect(city.houses[1].address.number).toBe(100);
+//   expect(city.houses[1].address.street.title).toBe("Happy street");
+
+//   expect(city.houses[2].buildedAt).toBe(2020);
+//   expect(city.houses[2].repaired).toBe(false);
+//   expect(city.houses[2].address.number).toBe(200);
+//   expect(city.houses[2].address.street.title).toBe("Hogwarts street");
+// })
+
+export {}
---
+++ src/02.tsx
@@ -0,0 +1,19 @@
+// type StreetType = {
+//   title: string
+// }
+
+// type AddressType = {
+//   number: number
+//   street: StreetType
+// }
+
+// type HousesType = {
+//   buildedAt: number
+//   repaired: boolean
+//   address: AddressType
+// }
+
+// type Citytype = {
+//   houses: Array<HousesType>
+// }
+export {}
